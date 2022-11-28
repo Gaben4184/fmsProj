2cc8041f276c56322b9cb3701fd2ec873b2ff7bf
TheCoolestBerry <115311057+TheCoolestBerry@users.noreply.github.com>
2022-11-28 11:57:11 -0700

    Update color.js

--- dev/color.js
+++ dev/color.js
@@ -9,45 +9,46 @@ function draw() {
   if (mouseIsPressed == true) {
     fill(123, 63, 0);
   } else {
-    fill(255);
+    noFill(220);
   }
-  noStroke();
   ellipse(circX, circY, 120, 120);
+  noStroke();
+  fill(220);
+  ellipse(50, 400, 80,80);
 
   noStroke();
   fill(220);
-  circle(50, 400, 80);
-
-
-  circle(1555, 100, 30);
+  ellipse(1555, 100, 30,30);
 
   noStroke();
   fill(220);
-  circle(350, 480, 30);
+  ellipse(350, 480, 30,30);
 
   noStroke();
   fill(220);
-  circle(700, 800, 150);
+  ellipse(700, 800, 150,150);
 
   noStroke();
   fill(220);
-  circle(800, 600, 50);
+  ellipse(800, 600, 50,50);
 
   noStroke();
   fill(220);
-  circle(1300, 380, 100);
+  ellipse(1300, 380, 100,100);
 
   noStroke();
   fill(220);
-  circle(1000, 380, 150);
-  fill(0);
+  ellipse(1000, 380, 150,150);
 
   button = createButton("If you find all 7 circles, click here to restart");
   button.position(150, 1);
   button.size(150, 100);
   button.mousePressed(res);
-}
 
+  fill(0);
+  
 function res() {
   location.reload();
+}
+
 }
