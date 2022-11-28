027cd7486957de372620b7daf3291e2538508cd5
Gaben4184 <grodfam4@gmail.com>
2022-11-28 11:31:20 -0700

    idk changes

--- dev/color.js
+++ dev/color.js
@@ -3,59 +3,53 @@ function setup() {
 }
 
 function draw() {
-  
   let circX = mouseX;
   let circY = mouseY;
-  
-  if(mouseIsPressed == true) {
-    fill(123,63,0);
-  }
-  else{
+
+  if (mouseIsPressed == true) {
+    fill(123, 63, 0);
+  } else {
     noFill(220);
   }
-  ellipse(circX,circY,120,120);
+  ellipse(circX, circY, 120, 120);
   noStroke();
   fill(220);
-circle(50,400,80);
- 
-    noStroke();
-  fill(220);
-circle(1555,100,30);
- 
+  circle(50, 400, 80);
+
   noStroke();
   fill(220);
-circle(350,480,30);
- 
+  circle(1555, 100, 30);
+
   noStroke();
   fill(220);
-circle(700,800,150);
- 
+  circle(350, 480, 30);
+
   noStroke();
   fill(220);
-circle(800,600,50);
- 
+  circle(700, 800, 150);
+
   noStroke();
   fill(220);
-circle(1300,380,100);
- 
+  circle(800, 600, 50);
+
   noStroke();
   fill(220);
-circle(1000,380,150);
- 
-   button = createButton('If you find all 7 circles, click here to restart');
-        button.position(1, 1);
-        button.size(150, 100);
-        button.mousePressed(res);
+  circle(1300, 380, 100);
 
+  noStroke();
+  fill(220);
+  circle(1000, 380, 150);
 
-   fill(0);
+  button = createButton("If you find all 7 circles, click here to restart");
+  button.position(1, 1);
+  button.size(150, 100);
+  button.mousePressed(res);
 
-
-
- 
-function res(){
-    location.reload();
-
+  fill(0);
+  
+function res() {
+  location.reload();
 }
 
 }
+
