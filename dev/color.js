8787f9aaf532c83221ec915a91119b9703eb5e74
jnguy146 <115834231+jnguy146@users.noreply.github.com>
2022-11-28 11:02:59 -0800

    Update color.js

--- dev/color.js
+++ dev/color.js
@@ -1,56 +1,60 @@
+
+
 function setup() {
   createCanvas(1600, 900);
   background(220);
-  stroke(20);
-  fill(220);
-  circle(50, 400, 80);
-
-  stroke(20);
-  fill(220);
-  circle(1555, 100, 90);
-
-  stroke(20);
-  fill(220);
-  circle(350, 480, 90);
-
-  stroke(20);
-  fill(220);
-  circle(700, 800, 150);
-
-  stroke(20);
-  fill(220);
-  circle(800, 600, 50);
-
-  stroke(20);
-  fill(220);
-  circle(1300, 380, 100);
-
-  stroke(20);
-  fill(220);
-  circle(1000, 380, 150);
 }
 
 function draw() {
-  let circX = mouseX;
-  let circY = mouseY;
-
-  if (mouseIsPressed == true) {
-    fill(123, 63, 0);
-  } else {
+  if(mouseIsPressed == true) {
+    fill(123,63,0);
+  }
+  else{
     noFill(220);
   }
+  ellipse(mouseX,mouseY,120,120);
   noStroke();
-  ellipse(circX, circY, 50, 50);
-  
-  button = createButton("Once you color in all 7 circles, click here to restart");
-  button.position(150, 1);
-  button.size(150, 100);
-  button.mousePressed(res);
+  fill(220);
+circle(50,400,80);
+ 
+    noStroke();
+  fill(220);
+circle(1555,100,30);
+ 
+  noStroke();
+  fill(220);
+circle(350,480,30);
+ 
+  noStroke();
+  fill(220);
+circle(700,800,150);
+ 
+  stroke(20);
+  fill(220);
+Circle(800,600,50);
+ 
+  noStroke();
+  fill(220);
+circle(1300,380,100);
+ 
+  noStroke();
+  fill(220);
+circle(1000,380,150);
+ 
+   button = createButton('If you find all 7 circles, click here to restart');
+        button.position(1, 1);
+        button.size(150, 100);
+        button.mousePressed(res);
 
-  fill(0);
-  
-function res() {
-  location.reload();
+
+   fill(0);
+
+
+
+ 
+function res(){
+    location.reload();
+
 }
 
 }
